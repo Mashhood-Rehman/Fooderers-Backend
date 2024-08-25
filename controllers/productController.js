14ca099aec2b8a066f616bfbde25f04a2c2fc6eb
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-08-25 21:55:48 +0500

    My Restaurant Backend

---
+++ controllers/productController.js
@@ -0,0 +1,64 @@
+const productModel = require("../models/product");
+
+const productcreate = (req, res) => {
+  try {
+    const productregister = new productModel({
+      name: req.body.name,
+      amount: req.body.amount,
+      picture: req.body.picture,
+      price: req.body.price,
+      category: req.body.category,
+      description: req.body.description
+    });
+    productregister.save();
+    res.status(201).json({
+      success: true,
+      message: "product created successfully",
+      productregister,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+const getProducts = async (req, res) => {
+  try {
+    const products = await productModel.find();
+    res.status(200).json({ success: true, message: "products fetcheddd" , products });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await productModel.find();
+    res.status(200).json({ success: true, message: " All products fetcheddd", products });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const delProducts = async (req, res) => {
+  try {
+    const { id } = req.params; // Use 'id' here to match the route parameter
+    const deletedProduct = await productModel.findByIdAndDelete(id);
+    
+    if (!deletedProduct) {
+      return res.status(404).json({ success: false, message: `Product with id ${id} not found` });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Product with id ${id} has been deleted successfully`,
+      deletedProduct,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Unable to delete product", error });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  productcreate,
+  getProducts,
+  delProducts
+};
---
+++ index.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const bodyParser = require("body-parser")
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+
+
+
+const router = require("./routes/productroute");
+const route = require("./routes/userRoute");
+const orderRouter  =require("./routes/orderRoute")
+
+
+
+const stripe = require("stripe") ("sk_test_51PkqrUAusfi4SBU0ognZMrzTK9eJhstHcg0vKdqXIc2zS8RdEQMZuhNjhPlquk99TBHrfEto8FsmFZB9LS4PRCzC00sXgmu7lD")
+
+
+
+
+app.use(express.json())
+
+app.use(cors());
+
+
+app.use(bodyParser.json());
+
+
+
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("the db is running successfully"))
+  .catch((err) => console.log(err));
+
+
+
+
+  app.use("/", route);
+  app.use("/", router);
+app.use("/", orderRouter)
+app.post("/create-checkout-sessions", async (req, res) => {
+  try {
+    const { products } = req.body;
+
+    const lineItems = products.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name ,
+          // Image : [item.picture]
+                },
+        unit_amount: item.price*100 ,
+      },
+      quantity: item.quantity 
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: "http://localhost:5173/dispatch",
+      cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.json({ id: session.id });
+  } catch (error) {
+    res.status(500).send(`Error creating checkout session: ${error.message}`);
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});
